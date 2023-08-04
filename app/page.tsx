68ca843e8ab83db9eb3a8471e5ca630a90f14bfb
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-08-04 16:12:17 +0700

    Created Most loved, popular, and affordable section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 import Image from "next/image";
 import Button from "@/components/button";
 import Cards from "@/components/cards";
+import type { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "Ganesha Space",
+};
 
 const Home = () => {
-  // Lowest pricestart sorted ascending and sliced to 4.
-  const mostAffordable = [
+  // Highest review sorted descending and sliced to 4.
+  const mostPopular = [
     { id: "1", title: "Bosscha Space", url: "/itb.jpeg", review: 40000 },
     { id: "2", title: "Bosscha Space 2", url: "/itb.jpeg", review: 30000 },
     { id: "3", title: "Bosscha Space 3", url: "/itb.jpeg", review: 20000 },
     { id: "4", title: "Bosscha Space 3", url: "/itb.jpeg", review: 20000 },
+  ];
+
+  // Highest rating sorted descending sliced to 4
+  const mostLoved = [
+    { id: "1", title: "Bosscha Space", url: "/itb.jpeg", rating: 4.0 },
+    { id: "2", title: "Bosscha Space 2", url: "/itb.jpeg", rating: 4.4 },
+    { id: "3", title: "Bosscha Space 3", url: "/itb.jpeg", rating: 4.3 },
+    { id: "4", title: "Bosscha Space 3", url: "/itb.jpeg", rating: 4.2 },
+  ];
+
+  // Lowest price sorted ascending sliced to 4
+  const mostAffordable = [
+    { id: "1", title: "Bosscha Space", url: "/itb.jpeg", priceStart: 40000 },
+    { id: "2", title: "Bosscha Space 2", url: "/itb.jpeg", priceStart: 45000 },
+    { id: "3", title: "Bosscha Space 3", url: "/itb.jpeg", priceStart: 50000 },
+    { id: "4", title: "Bosscha Space 3", url: "/itb.jpeg", priceStart: 55000 },
   ];
 
   return (
@@ -17,12 +38,12 @@ const Home = () => {
       <section className="relative flex h-[calc(100vh-80px)] w-full items-center p-5 sm:p-10 xl:h-[calc(100vh-90px)] xl:p-20">
         <div className="z-10 flex max-w-4xl flex-col gap-4 xl:gap-7">
           {/* Title */}
-          <h1 className="font-poppins text-4xl font-bold text-white xl:text-6xl">
+          <h1 className="font-poppins text-4xl font-bold tracking-wide text-white xl:text-6xl">
             Yuk <span className="text-custom-blue-green">Nongkrong</span>{" "}
             Bareng!
           </h1>
           {/* Description */}
-          <p className="font-inter text-lg font-normal text-white xl:text-xl">
+          <p className="font-inter text-lg font-normal tracking-wider text-white xl:text-xl">
             Disini tersedia informasi mengenai Cafe dan Co-working space di
             sekitar ITB. Pas banget buat kalian yang suka nugas maupun belajar
             dan lagi nyari tempat yang aman, nyaman, dan deket!
@@ -42,16 +63,75 @@ const Home = () => {
         />
       </section>
 
-      <div className="p-6 lg:p-10 xl:p-16">
+      <div className="flex flex-col gap-8 p-6 sm:p-8 xl:gap-16 xl:p-16">
         {/* Most Popular */}
-        <section className="flex flex-col gap-4">
+        <section className="flex flex-col gap-4 xl:gap-8">
+          {/* Title */}
+          <h2 className="font-poppins text-2xl font-bold text-white xl:text-4xl">
+            Most <span className="text-custom-blue-green">Popular</span>
+          </h2>
+
+          {/* Grids */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3 xl:grid-cols-4 xl:gap-12">
+            {mostPopular.map((item) => {
+              return (
+                <Cards
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  url={item.url}
+                  review={item.review}
+                />
+              );
+            })}
+          </div>
         </section>
 
         {/* Highest Rating */}
-        <section></section>
+        <section className="flex flex-col gap-4 xl:gap-8">
+          {/* Title */}
+          <h2 className="font-poppins text-2xl font-bold text-white xl:text-4xl">
+            Most <span className="text-red-500">Loved</span>
+          </h2>
+
+          {/* Grids */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3 xl:grid-cols-4 xl:gap-12">
+            {mostLoved.map((item) => {
+              return (
+                <Cards
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  url={item.url}
+                  rating={item.rating}
+                />
+              );
+            })}
+          </div>
+        </section>
 
         {/* Most affordable */}
-        <section></section>
+        <section className="flex flex-col gap-4 xl:gap-8">
+          {/* Title */}
+          <h2 className="font-poppins text-2xl font-bold text-white xl:text-4xl">
+            Most <span className="text-green-400">Affordable</span>
+          </h2>
+
+          {/* Grids */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3 xl:grid-cols-4 xl:gap-12">
+            {mostAffordable.map((item) => {
+              return (
+                <Cards
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  url={item.url}
+                  priceStart={item.priceStart}
+                />
+              );
+            })}
+          </div>
+        </section>
 
         {/* Our Recommendation */}
         <section></section>
