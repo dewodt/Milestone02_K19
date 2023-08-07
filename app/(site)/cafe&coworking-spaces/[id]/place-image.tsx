99df261677767827a08895486c8a3bd74123ae3f
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-07 11:35:22 +0700

    feat(image-details): Make carousels to show details of image

---
+++ app/(site)/cafe&coworking-spaces/[id]/place-image.tsx
@@ -0,0 +1,122 @@
+"use client"
+import { Place } from '@/types/cms'
+import React from 'react'
+import { urlForImage } from "@/sanity/lib/image";
+import { useContext } from "react";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import Image from 'next/image';
+import { PopUpContext } from '../../layout';
+import PopUpClient from './popup-client';
+const PlaceImage = ({ place }: { place: Place }) => {
+
+    const setPopUp = useContext(PopUpContext) as React.Dispatch<
+        React.SetStateAction<React.ReactNode | undefined>
+    >;
+    return (
+        <section className="flex h-[150px] w-full gap-2 md:h-[280px] md:gap-4 lg:h-[500px] lg:gap-6">
+            {/* First Image */}
+            {place.images[0] && (
+                <button onClick={() => {
+                    setPopUp(<PopUpClient place={place} initialSlide={0} />)
+                }}
+                    className={`${place.images.length === 1
+                        ? "w-full"
+                        : "w-[calc(50%-6px)] md:w-[calc(50%-8px)] lg:w-[calc(50%-12px)]"
+                        } h-full `}>
+                    <Image
+                        src={urlForImage(place.images[0]).url()}
+                        width="1920"
+                        height={1080}
+                        className={`w-full h-full rounded-xl object-cover object-center`}
+                        alt={place.images[0].alt}
+                    />
+                </button>
+            )}
+            <div className="flex flex-1 flex-col gap-2 md:gap-4 lg:gap-6">
+                {/* Container 2nd and 3rd */}
+                <div
+                    className={`flex w-full ${place.images.length > 3 ? "flex-row" : "flex-col"
+                        } gap-2 md:gap-4 lg:gap-6 ${place.images.length > 3
+                            ? "h-[calc(50%-6px)] md:h-[calc(50%-8px)] lg:h-[calc(50%-12px)]"
+                            :"h-full"
+                        }`}
+                >
+                    {place.images[1] && (
+                        <button onClick={() => {
+                            setPopUp(<PopUpClient place={place} initialSlide={1} />)
+                        }} className={`${place.images.length > 3
+                            ? "w-[calc(50%-6px)] md:w-[calc(50%-8px)] lg:w-[calc(50%-12px)] h-full"
+                            : place.images.length ===3?"w-full h-1/2":"w-full h-full"
+                            } `}>
+                            <Image
+                                src={urlForImage(place.images[1]).url()}
+                                width="1920"
+                                height={1080}
+                                className="h-full rounded-xl object-cover object-center w-full"
+                                alt={place.images[1].alt}
+                            /></button>
+                    )}
+                    {place.images[2] && (
+                        <button onClick={() => {
+                            setPopUp(<PopUpClient place={place} initialSlide={2} />)
+                        }} className={`${place.images.length === 3
+                            ? "w-full h-[calc(50%-6px)] md:h-[calc(50%-8px)] lg:h-[calc(50%-12px)]"
+                            : "w-[calc(50%-6px)] md:w-[calc(50%-8px)] lg:w-[calc(50%-12px)] h-full"
+                            } `}>
+                            <Image
+                                src={urlForImage(place.images[2]).url()}
+                                width="1920"
+                                height={1080}
+                                className="w-full h-full rounded-xl object-cover object-center"
+                                alt={place.images[2].alt}
+                            />
+                        </button>
+                    )}
+                </div>
+                {/* Container 4th and 5th */}
+                <div className="flex h-[calc(50%-6px)] w-full gap-2 md:h-[calc(50%-8px)] md:gap-4 lg:h-[calc(50%-12px)] lg:gap-6">
+                    {place.images[3] && (
+                        <button onClick={() => {
+                            setPopUp(<PopUpClient place={place} initialSlide={3} />)
+                        }} className={`${place.images.length === 4
+                            ? "w-full"
+                            : "w-[calc(50%-6px)] md:w-[calc(50%-8px)] lg:w-[calc(50%-12px)]"
+                            }`}>
+                            <Image
+                                src={urlForImage(place.images[3]).url()}
+                                width="1920"
+                                height={1080}
+                                className="w-full h-full rounded-xl object-cover object-center"
+                                alt={place.images[3].alt}
+                            />
+                        </button>
+                    )}
+                    {place.images[4] && (
+                        <button onClick={() => {
+                            setPopUp(<PopUpClient place={place} initialSlide={4} />)
+                        }} className="relative w-[calc(50%-6px)] md:w-[calc(50%-8px)] lg:w-[calc(50%-12px)]">
+                            <Image
+                                src={urlForImage(place.images[4]).url()}
+                                width="1920"
+                                height={1080}
+                                className="h-full w-full rounded-xl object-cover object-center"
+                                alt={place.images[4].alt}
+                            />
+                            {place.images.length > 5 &&
+                                <>
+                                    <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                                    <p className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-white z-20 font-poppins font-bold text-4xl">{place.images.length - 5}+</p>
+                                </>}
+                        </button>
+                    )}
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PlaceImage
---
+++ app/(site)/cafe&coworking-spaces/[id]/popup-client.tsx
@@ -0,0 +1,117 @@
+"use client"
+import { Place } from "@/types/cms"
+import React from "react"
+import { PortableText, toPlainText } from "@portabletext/react";
+import { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper as SwiperType } from "swiper";
+import { urlForImage } from "@/sanity/lib/image";
+import { useContext } from "react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import Image from "next/image";
+
+import CircleCrossIcon from "@/components/icons/circle-cross-icon";
+import { PopUpContext } from "../../layout";
+import CrossIcon from "@/components/icons/cross-icon";
+const PopUpClient = ({ place, initialSlide }: { place: Place, initialSlide: number }) => {
+    const setPopUp = useContext(PopUpContext) as React.Dispatch<
+        React.SetStateAction<React.ReactNode | undefined>
+    >;
+    // Get formatted values
+    const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+    const swiperRef = useRef<any>(null);
+
+
+    // Function to handle Swiper instance update
+    // Function to handle Swiper instance update
+    const handleThumbsSwiperUpdate = (swiper: SwiperType | null) => {
+        setThumbsSwiper(swiper);
+    };
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <div className={`h-screen bg-custom-soft-black w-screen transition-opacity duration-300 overflow-hidden fixed z-[999]`}>
+            <>
+                <div className="bg-black w-screen flex items-center justify-between p-5 lg:py-10 lg:px-20 absolute top-0 z-[99]">
+                    <h1 className="text-white text-xl break-all lg:text-4xl font-bold font-poppins">{place.name}</h1>
+                    <button onClick={() => setPopUp(undefined)} className="aspect-square rounded-full ">
+                        <CrossIcon size={160} className="fill-white w-8 h-8 lg:w-14 lg:h-14" />
+                    </button>
+                </div>
+                <Swiper
+                    loop={true}
+                    spaceBetween={10}
+                    navigation={true}
+                    thumbs={{ swiper: thumbsSwiper }}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper2"
+                    initialSlide={initialSlide}
+                >
+                    {place.images.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <Image alt={`Image ${index + 1}`} width={1920} height={1080} src={urlForImage(item).url()} className="w-screen object-center object-contain h-[calc(100vh-170px)]" />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="bg-black w-screen bg-opacity-50 m-auto px-10 overflow-x-scroll md:px-28 lg:px-32 h-[200px]">
+                    <Swiper
+                        ref={swiperRef}
+                        onSwiper={handleThumbsSwiperUpdate}
+                        loop={true}
+                        centeredSlides={true}
+                        centerInsufficientSlides={true}
+                        centeredSlidesBounds={true}
+                        breakpoints={{
+                            320: {
+                              slidesPerView: 2,
+                              spaceBetween:0
+                            },
+                            480: {
+                                slidesPerView: 2,
+                                spaceBetween:0
+                            },
+                            640: {
+                                slidesPerView: 2,
+                                spaceBetween:5
+                            },
+                            720:{
+                                slidesPerView: 5,
+                                spaceBetween:5
+                            },
+                            1280:{
+                                slidesPerView: 5,
+                                spaceBetween:10
+                                
+                            }
+                          }}
+                        
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="mySwiper"
+                    >
+                        {place.images.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <Image alt={`Image ${index + 1}`} width={1920} height={1080} src={urlForImage(item).url()} className={`my-auto w-[100px] h-[80px] lg:h-[120px] cursor-pointer rounded-[20px] border-2  lg:w-[300px] object-center object-cover`} />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </>
+        </div>
+    );
+}
+
+export default PopUpClient
